4652be707b93f91a94f345999cd727888520978c
leslie sayegh <leslie.sayegh@laplateforme.io>
2023-04-25 18:51:41 +0200

    classe methode information véhicule

--- Jour02/job06/main.py
+++ Jour02/job06/main.py
@@ -6,38 +6,40 @@ class Vehicule:
         self.prix = prix
 
     def informationsVehicule(self):
-        print("Marque : ", self.marque)
-        print("Modèle : ", self.modele)
-        print("Année : ", self.annee)
-        print("Prix : ", self.prix)
+        print("Marque :", self.marque)
+        print("Modèle :", self.modele)
+        print("Année :", self.annee)
+        print("Prix :", self.prix, "euros")
 
-class Voiture(Vehicule):
-    def __init__(self, marque, modele, annee, prix, portes=4):
-        super().__init__(marque, modele, annee, prix)
-        self.portes = portes
-
-    def informationsVehicule(self):
-        super().informationsVehicule()
-        print("Nombre de portes : ", self.portes)
-
-# Création d'un objet de la classe "Vehicule"
-vehicule1 = Vehicule("Peugeot", "308", 2022, 25000)
+# Création d'une instance de Vehicule
+mon_vehicule = Vehicule("Peugeot", "208", 2019, 15000)
 
 # Affichage des informations du véhicule
-vehicule1.informationsVehicule()
+mon_vehicule.informationsVehicule()
 
-# Création d'un objet de la classe "Voiture"
-voiture1 = Voiture("Renault", "Clio", 2023, 18000)
 
-# Affichage des informations de la voiture
-voiture1.informationsVehicule()
+class Voiture(Vehicule):
+    def __init__(self, marque, modele, annee, prix):
+        super().__init__(marque, modele, annee, prix)
+        self.portes = 4
+    
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print("Nombre de portes :", self.portes)
 
-Marque :  Peugeot
-Modèle :  308
-Année :  2022
-Prix :  25000
-Marque :  Renault
-Modèle :  Clio
-Année :  2023
-Prix :  18000
-Nombre de portes :  4
+ma_voiture = Voiture("Mercedes", "Classe A", 2020, 18500)
+ma_voiture.informationsVehicule()
+
+class Moto(Vehicule):
+    def __init__(self, marque, modele, annee, prix):
+        super().__init__(marque, modele, annee, prix)
+        self.roues = 2
+    
+    def informationsVehicule(self):
+        super().informationsVehicule()
+        print("Nombre de roues :", self.roues)
+
+ma_moto = Moto("Yamaha", "1200 Vmax", 1987, 4500)
+ma_moto.informationsVehicule()
+
+
