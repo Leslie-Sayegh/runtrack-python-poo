9b7fc82f552f43030c8960c6ddf53ed7314ce850
leslie sayegh <leslie.sayegh@laplateforme.io>
2023-04-25 02:13:15 +0200

    ascenseur_get_set

--- Jour01/job06/main.py
+++ Jour01/job06/main.py
@@ -1,34 +1,37 @@
-class Rectangle:
+#classe, ascenseur, mutateur
 
-    def __init__(self, longueur, largeur):
-        self.__longueur = longueur
-        self.__largeur = largeur
-        
-    def get_longueur(self):
-        return self.__longueur
+class Rectangle :
+
+    def __init__(self, longueur, largeur) :
+      self.longueur = longueur
+      self.largeur = largeur
+    def get_longueur(self) :
+       return self.longueur
     
+    def set_longueur(self, longueur) :
+       self.longueur = longueur
     
-    def set_longueur(self, longueur):
-        self.__longueur = longueur
-        
-    def get_largeur(self):
-        return self.__largeur
+    def get_largeur(self) :
+       return self.largeur
     
-    def set_largeur(self, largeur):
-        self.__largeur = largeur
+    def set_largeur(self, largeur) :
+       self.largeur = largeur
+
+#création rectangle
+mesure = Rectangle (10, 5)
+
+#affichage mesure
+print ("Longueur :", mesure.get_longueur())
+print ("Largeur :", mesure.get_largeur())
+
+#modification mesure
+mesure.set_longueur(6)
+mesure.set_largeur(3)
+
+#affichage mesure modif
+print("Longueur modifiée :", mesure.get_longueur())
+print("Largeur modifiée :", mesure.get_largeur())
 
 
-# Création d'un rectangle de longueur 10 et de largeur 5
-        rectangle = Rectangle(10, 5)
 
 
-# Affichage de la longueur et de la largeur du rectangle
-        print("Longueur :", rectangle.get_longueur())
-        print("Largeur :", rectangle.get_largeur())
-
-# Modification de la longueur et de la largeur du rectangle
-        rectangle.set_longueur(6)
-        rectangle.set_largeur(3)
-
-# Affichage de la longueur et de la largeur du rectangle après modification
-        print("Longueur modifiée :", rectangle.get_longueur())
